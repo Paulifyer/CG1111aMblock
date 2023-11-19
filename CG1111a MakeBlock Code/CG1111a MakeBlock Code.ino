c2484e48c5864076b2a79016857969cdf33477b2
EthanSoh <ethansoh0308@gmail.com>
2023-11-19 19:24:38 +0800

    Updated to reflect the code during the final run

--- CG1111a MakeBlock Code/CG1111a MakeBlock Code.ino
+++ CG1111a MakeBlock Code/CG1111a MakeBlock Code.ino
@@ -8,8 +8,6 @@
 #define LDRWait 10  //in milliseconds
 #define LDR A0      //LDR sensor pin at A0
 #define LED 13      //Check Indicator to signal Calibration Completed
-// Define colour sensor LED pins
-//placeholders for colour detected
 MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
 MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
 MeRGBLed led(0,30);
@@ -20,10 +18,59 @@ uint8_t motorSpeed = 255;
 
 //floats to hold colour arrays
 float colourArray[] = { 0, 0, 0 };
-float whiteArray[] = { 977, 990, 959 };
-float blackArray[] = { 942, 972, 847 };
-float greyDiff[] = { 35, 18, 112 };
+float whiteArray[] = { 968, 974, 941 };
+float blackArray[] = { 927, 926, 895 };
+float greyDiff[] = { 16, 37, 43 };
 char colourStr[3][5] = { "R = ", "G = ", "B = " };
+
+void celebrate() {// Code for playing celebratory tune}
+// Each of the following "function calls" plays a single tone.
+// The numbers in the bracket specify the frequency and the duration (ms)
+buzzer.tone(190, 200);
+buzzer.tone(220, 200);
+buzzer.tone(220, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(739.99, 200);
+buzzer.tone(739.99, 200);
+buzzer.tone(880, 200);
+buzzer.tone(185, 200);
+buzzer.tone(220, 200);
+buzzer.tone(220, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(740, 200);
+buzzer.tone(740, 200);
+buzzer.tone(880, 200);
+buzzer.tone(987.77, 600);
+buzzer.tone(783.99, 150);
+buzzer.tone(1174.66, 1600);
+buzzer.tone(185, 200);
+buzzer.tone(220, 200);
+buzzer.tone(220, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(739.99, 200);
+buzzer.tone(739.99, 200);
+buzzer.tone(880, 200);
+buzzer.tone(185, 200);
+buzzer.tone(220, 200);
+buzzer.tone(220, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(587.33, 200);
+buzzer.tone(740, 200);
+buzzer.tone(740, 200);
+buzzer.tone(880, 200);
+buzzer.tone(1046.50, 500);
+buzzer.tone(1046.50, 200);
+buzzer.tone(1046.50, 1200);
+buzzer.tone(1046.50, 200);
+buzzer.tone(987.77, 500);
+buzzer.tone(1046.50, 150);
+buzzer.tone(783.99, 1000);
+buzzer.noTone();
+}
+
 
 void set_output() {
   pinMode(A2, OUTPUT);
@@ -35,24 +82,24 @@ void turnoff() {
   digitalWrite(A3, LOW);
 }
 
-void turnred() {
+void shineRed() {
   digitalWrite(A3, HIGH);
   digitalWrite(A2, HIGH);
 }
 
-void turngreen() {
+void shineGreen() {
   digitalWrite(A2, HIGH);
   digitalWrite(A3, LOW);
 }
 
-void turnblue() {
+void shineBlue() {
   digitalWrite(A3, HIGH);
   digitalWrite(A2, LOW);
 }
 
 float measureAmbient()
 {
-  turnred();
+  shineRed();
   delay(10);
   return analogRead(A1);
 }
@@ -65,30 +112,30 @@ float measureIR()
   return analogRead(A1);
 }
 
-void stopMotor() {// Code for stopping motor}
+void stopMotor() {// Codfe for stopping motor}
 leftMotor.stop();
 rightMotor.stop();
 delay(1000);
 }
 
 void moveForward() {// Code for moving forward for some short interval}
-leftMotor.run(-motorSpeed);
+leftMotor.run(-230);
 rightMotor.run(motorSpeed);
 }
 
-void nudgeLeft() {// Code for nudging slightly to the left for some short interval}
-leftMotor.run(-motorSpeed + 40);
-rightMotor.run(motorSpeed);
-delay(3);
+void nudgeLeft() {// Code for nudging slightly to the left for some short interval
+leftMotor.run(-120);
+rightMotor.run(255);
+delay(50);
 }
 
-void nudgeRight() {// Code for nudging slightly to the right for some short interval}
-rightMotor.run(motorSpeed - 40);
-leftMotor.run(-motorSpeed);
-delay(3);
+void nudgeRight() {// Code for nudging slightly to the right for some short interval
+rightMotor.run(120);
+leftMotor.run(-255);
+delay(50);
 }
 
-void turnRight() {// Code for turning right 90 deg}
+void turnRight() {// Code for turning right 90 deg
 leftMotor.run(-motorSpeed * 0.8); // Positive: wheel turns anti-clockwise
 rightMotor.run(-motorSpeed * 0.8); // Positive: wheel turns anti-clockwise
 delay(TURNING_TIME_MS); // Keep turning right for this time duration
@@ -96,14 +143,13 @@ leftMotor.stop(); // Stop left motor
 rightMotor.stop(); // Stop right motor
 }
 
-void turnLeft() {// Code for turning left 90 deg}
+void turnLeft() {// Code for turning left 90 deg
 leftMotor.run(motorSpeed * 0.8); // Positive: wheel turns clockwise
 rightMotor.run(motorSpeed * 0.8); // Positive: wheel turns clockwise
 delay(TURNING_TIME_MS); // Keep turning left for this time duration
 leftMotor.stop(); // Stop left motor
 rightMotor.stop(); // Stop right motor
 }
-
 
 char check_colour(float colourArray[])
 {
@@ -160,32 +206,36 @@ void moveByColor(char color)
   {
     turnLeft();
     moveForward();
-    delay(500);
+    delay(700);
+    stopMotor();
     turnLeft();
   }
   else if (color == 'B')
   {
     turnRight();
     moveForward();
-    delay(500);
+    delay(780);
+    stopMotor();
     turnRight();
   }
   else if(color == 'W')
   {
     stopMotor();
+    celebrate();
+    delay(2000);
   }
 }
 
-void colourSensor()
+void colourSensor(float blackArray[], float greyDiff[])
 {
    for (int c = 0; c <= 2; c++) {
    Serial.print(colourStr[c]);
    if (c == 0) {
-     turnred();
+     shineRed();
    } else if (c == 1) {
-     turngreen();
+     shineGreen();
    } else {
-     turnblue();
+     shineBlue();
    }
    delay(RGBWait);
     // get the average of 5 consecutive readings for the current colour and return an average
@@ -204,22 +254,22 @@ void lineSensor() {
 int sensorState = lineFinder.readSensors();  
 if (sensorState == S1_IN_S2_IN)
 {
-  stopMotor();
-  colourSensor();
+  stopMotor(); // Stop motor and activate the color sensor
+  colourSensor(blackArray, greyDiff);
 }
 else if (sensorState == S1_OUT_S2_IN) {
-    leftMotor.run(-100); // Left wheel stops
-    rightMotor.run(0); // Right wheel go forward
+    leftMotor.run(-100); // Left wheel moves forward
+    rightMotor.run(0); // Right wheel stops
 }
 else if (sensorState == S1_IN_S2_OUT) {
-    leftMotor.run(0); // Left wheel go forward
-    rightMotor.run(100); // Right wheel stops    
+    leftMotor.run(0); // Left wheel stops
+    rightMotor.run(100); // Right wheel moves forward    
 }
   else if (sensorState == S1_OUT_S2_OUT){
-    return;
+     moveForward(); // continue moving forward if the line detector still detects color
+     return;
   }
-  sensorState = lineFinder.readSensors();
-  delay(20);
+  delay(50);
 }
 
 int checkDistanceUR()
@@ -231,7 +281,7 @@ int checkDistanceUR()
   delayMicroseconds(10);
   digitalWrite(ULTRASONIC, LOW);
   pinMode(ULTRASONIC, INPUT);
-  long duration = pulseIn(ULTRASONIC, HIGH, 2000);
+  long duration = pulseIn(ULTRASONIC, HIGH, 4000);
   return duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100;
 }
 
@@ -242,48 +292,49 @@ void setup() {
   }
   led.setpin(13);  //Check Indicator -- OFF during Calibration
   pinMode(A1, INPUT);
-  //begin serial communication
-  Serial.begin(9600);
-  // setBalance();             //calibration
+  Serial.begin(9600); //begin serial communication
+  // setBalance(); // code to fill the black, white and grey arrays
   led.show();  //Check Indicator -- ON after Calibration
-  delay(5000);
+  delay(200);
 }
 void loop() {
+  lineSensor();
   float ambient = 1024 - measureAmbient();
   Serial.println(ambient);
-  float IR1 = measureIR();
+  float IR1 = (measureIR() - ambient) / 1024 * 5;
   Serial.println(IR1);
-  delay(500);
-  if (IR1 < 100)
-  {
-    Serial.println("Nudging Left");
-    // nudgeLeft();
-  }
-  lineSensor();
-  moveForward();
+  delay(20);  
   int leftDist = checkDistanceUR();
-  Serial.println(leftDist);
-  if (leftDist <= 7 && leftDist > 0)
+  // Serial.println(leftDist);
+  if (leftDist <= 6 && leftDist > 0)
   {
-    Serial.println("Nudging Right");
+    // Serial.println("Nudging Right");
+    led.setColorAt(1, 255, 255, 255);
+    led.show();
     nudgeRight();
   }
-
+  else if ((leftDist > 16 && leftDist <= 23) || IR1 < 3 ) // Nudge Left either when the ultrasonic sensor detects that the left distance is too far or IR sensor detects distance too small
+  {
+   // Serial.println("Nudging Left");
+    led.setColorAt(0, 255, 255, 255);
+    led.show();
+    nudgeLeft();
+  }
+  led.setColor(0, 0, 0);
+  led.show();
+  delay(10);
 }
 void setBalance() {
-  //set white balance
   Serial.println("Put White Sample For Calibration ...");
   delay(5000);             //delay for five seconds for getting sample ready
   digitalWrite(LED, LOW);  //Check Indicator OFF during Calibration
-  //scan the white sample.
-//go through one colour at a time, set the maximum reading for each colour -- red, green and blue to the white array
   for (int c = 0; c <= 2; c++) {
     if (c == 0) {
-      turnred();
+      shineRed();
     } else if (c == 1) {
-      turngreen();
+      shineGreen();
     } else {
-      turnblue();
+      shineBlue();
     }
     delay(RGBWait);
     whiteArray[c] = getAvgReading(5);  //scan 5 times and return the average,
@@ -291,18 +342,17 @@ void setBalance() {
     turnoff();
     delay(RGBWait);
   }
-  //done scanning white, time for the black sample.
   //set black balance
   Serial.println("Put Black Sample For Calibration ...");
   delay(5000);  //delay for five seconds for getting sample ready
   //go through one colour at a time, set the minimum reading for red, green and blue to the black array
   for (int c = 0; c <= 2; c++) {
     if (c == 0) {
-      turnred();
+      shineRed();
     } else if (c == 1) {
-      turngreen();
+      shineGreen();
     } else {
-      turnblue();
+      shineBlue();
     }
     delay(RGBWait);
     blackArray[c] = getAvgReading(5);
@@ -316,7 +366,7 @@ void setBalance() {
   }
   //delay another 5 seconds for getting ready colour objects
   Serial.println("Colour Sensor Is Ready.");
-  delay(5000);
+  delay(10000);
 }
 int getAvgReading(int times) {
   //find the average reading for the requested number of times of scanning LDR
